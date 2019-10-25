960c83455de289a2d22c90d392780475baa1923d
YeonjiLim <reusepaper210@gmail.com>
2019-10-25 17:48:40 +0900

    [#1] S1P2151001-6 필요없는 파일들 삭제

--- ctf_frontend/src/App.vue
+++ ctf_frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CTFMainPage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CTFMainPage from './views/CTFMainPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CTFMainPage
   }
 }
 </script>
