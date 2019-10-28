0cdf867a878bc7b641d075bf5899e196832e8ac0
YeonjiLim <reusepaper210@gmail.com>
2019-10-28 13:31:16 +0900

    [#1] S1P2151001-6 메인페이지 header, navbar 영역 구분

--- ctf_frontend/src/components/CTFCode.vue
+++ ctf_frontend/src/components/CTFCode.vue
@@ -1,13 +1,13 @@
-<<template>
-    <div>
+<template>
+  <div>
 
-    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+  export default {
+    
+  }
 </script>
 
 <style lang="scss" scoped>
