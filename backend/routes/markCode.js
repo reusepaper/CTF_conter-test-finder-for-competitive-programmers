45e78a7443b0cf0476f178b4da7d8a8757957b50
박한범 <hb1398@naver.com>
2019-11-04 14:29:01 +0900

    채점기능 추가

--- backend/routes/markCode.js
+++ backend/routes/markCode.js
@@ -6,6 +6,7 @@ var dbconn = dbConObj.init();
 var router = express.Router();
 var  mysql = require('mysql');
 let crypto = require('crypto');
+var exec = require('child_process').exec;
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   var userInputCode=req.body.userInputCode;
@@ -20,11 +21,24 @@ router.post('/', function(req, res, next) {
 
 
   console.log({userInputCode:userInputCode,rightInputCode:rightInputCode});
-  
+
+  var child;
+
+command = "judge "
+source = "'"+userInputCode+"'"; 
+
+  child = exec(command + source,
+    function (error, stdout, stderr) {
+      var result = stdout
+      result = JSON.parse(result);
+      res.json(result);
+      console.log(result)
+  })
   var userResult="Hello world!";
   var rightResult="Hello world!2";
   var testcase="Hello world!!";
-  res.json({testcase:testcase,user:userResult,right:rightResult});
+  
+  // res.json({testcase:testcase,user:userResult,right:rightResult});
 
 });
 
